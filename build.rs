9078f46c769c34c844a4b8ee416c21242de22a7c
lcxl <lcx87654321@163.com>
2026-03-15 18:42:12 +0800

    feat(windows): automate libvpx download and setup during build
    - Add eqwest and zip to build dependencies for downloading and extracting prebuilt binaries.
    - Implement automatic download of libvpx for Windows when VPX_LIB_DIR is not set.
    - Update uild.rs to support version-specific prebuilt binaries (v1.12.0 and v1.13.0).
    - Link against libvpx statically on Windows.
    - Add generated FFI bindings for v1.14.0 and v1.15.2.

--- build.rs
+++ build.rs
@@ -7,8 +7,11 @@ pub fn main() {
     println!("cargo:rerun-if-env-changed=VPX_INCLUDE_DIR");
     println!("cargo:rerun-if-env-changed=VPX_STATIC");
     println!("cargo:rerun-if-env-changed=VPX_DYNAMIC");
+    println!("cargo:rerun-if-changed=build.rs");
 
-    let requested_version = env::var("VPX_VERSION").ok();
+    #[allow(unused_assignments)]
+    #[allow(unused_mut)]
+    let mut requested_version = env::var("VPX_VERSION").ok();
 
     let src_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let src_dir = Path::new(&src_dir);
@@ -20,8 +23,60 @@ pub fn main() {
         out_dir.join("ffi.rs")
     };
 
+    #[allow(unused_assignments)]
+    #[allow(unused_mut)]
+    let mut vpx_lib_dir = env::var_os("VPX_LIB_DIR");
+    #[cfg(target_os = "windows")]
+    {
+        let donwload_url;
+        // On Windows, if the user specified a version, we append it to the lib dir.
+        #[cfg(feature = "vpx_1_13")]
+        {
+            donwload_url = "https://github.com/ShiftMediaProject/libvpx/releases/download/v1.13.0/libvpx_v1.13.0_msvc16.zip";
+            requested_version = Some("1.13.0".to_string());
+        }
+
+        #[cfg(feature = "vpx_1_12")]
+        {
+            donwload_url = "https://github.com/ShiftMediaProject/libvpx/releases/download/v1.12.0/libvpx_v1.12.0_msvc16.zip";
+            requested_version = Some("1.12.0".to_string());
+        }
+
+        // If VPX_LIB_DIR is not set, we download a prebuilt libvpx and use that.
+        let out_dir = env::var_os("OUT_DIR").unwrap();
+        let out_dir = Path::new(&out_dir);
+        let download_dir = out_dir.join("libvpx-download");
+        let download_stamp = out_dir.join("libvpx-download.stamp");
+        if !download_stamp.exists() {
+            if download_dir.exists() {
+                std::fs::remove_dir_all(&download_dir).unwrap();
+            }
+            std::fs::create_dir_all(&download_dir).unwrap();
+
+            let zip_path = download_dir.join("libvpx.zip");
+            {
+                let mut resp = reqwest::blocking::get(donwload_url).unwrap();
+                assert!(resp.status().is_success());
+                let mut out = std::fs::File::create(&zip_path).unwrap();
+                std::io::copy(&mut resp, &mut out).unwrap();
+            }
+
+            let mut zip = zip::ZipArchive::new(std::fs::File::open(&zip_path).unwrap())
+                .expect("Failed to read libvpx zip file");
+            zip.extract(&download_dir)
+                .expect("Failed to extract libvpx zip file");
+
+            std::fs::write(&download_stamp, b"downloaded")
+                .expect("Failed to write libvpx download stamp");
+        }
+
+        // The extracted folder name is known from the zip contents.
+        let libdir = download_dir.join("lib").join("x64");
+        assert!(libdir.exists(), "Downloaded libvpx lib dir not found");
+        vpx_lib_dir = Some(libdir.into());
+    }
     #[allow(unused_variables)]
-    let (found_version, include_paths) = match env::var_os("VPX_LIB_DIR") {
+    let (found_version, include_paths) = match vpx_lib_dir {
         None => {
             // use VPX config from pkg-config
             let lib = pkg_config::probe_library("vpx").unwrap();
@@ -48,13 +103,17 @@ pub fn main() {
             let statik = infer_static("VPX");
 
             // Set libname.
-            if statik {
-                #[cfg(target_os = "windows")]
-                println!("cargo:rustc-link-lib=static=libvpx");
-                #[cfg(not(target_os = "windows"))]
-                println!("cargo:rustc-link-lib=static=vpx");
-            } else {
-                println!("cargo:rustc-link-lib=vpx");
+            // windows is always static link for now
+            #[cfg(target_os = "windows")]
+            println!("cargo:rustc-link-lib=static=libvpx");
+
+            #[cfg(not(target_os = "windows"))]
+            {
+                if statik {
+                    println!("cargo:rustc-link-lib=static=vpx");
+                } else {
+                    println!("cargo:rustc-link-lib=vpx");
+                }
             }
 
             let mut include_paths = vec![];
